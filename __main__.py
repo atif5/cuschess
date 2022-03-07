c230d386e28d872b53c1ff408513a86a8dbe1137
Atıf Aydın Turanlı <aydin.turanli@sabanciuniv.edu>
2022-03-08 00:08:01 +0300

    server

--- __main__.py
+++ __main__.py
@@ -1,9 +1,14 @@
 import sys
+import os
+from cuschess.cuserver.server import main as serv
 from cuschess.computer.__main__ import main as comp
 from cuschess.chess.__main__ import main as twoplayer
 from cuschess.cusonline.__main__ import main as online
 
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
 map_ = {
+    "--server": serv,
     "--online": online,
     "--2player": twoplayer,
     "--computer": comp
